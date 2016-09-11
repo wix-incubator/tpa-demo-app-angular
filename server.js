51e2d088e5efe7a8e43c497690cb4988646949cb
Adiel <adiela@wix.com>
2016-09-11 14:54:19 +0300

    adding Dor Galam's demo app for review

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var express = require('express');
+var app = express();
 
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true
-}).listen(5000, 'localhost', function (err, result) {
-    if (err) {
-        return console.log(err);
-    }
-
-    console.log('Listening at http://localhost:5000/');
-});
+app.use(express.static('.'));
+console.log('Listening on port 9000');
+app.listen(9000);
