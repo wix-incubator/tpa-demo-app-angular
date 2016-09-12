39d071a13b2844b3ccce364738e6cffc616e31e9
dorg11 <dorg@wix.com>
2016-09-12 14:03:31 +0300

    removed testing commands

--- src/settings/app.js
+++ src/settings/app.js
@@ -2,7 +2,6 @@ var app = angular.module('settings', ['WixControls']);
 
 app.controller('controller', ['$scope', function($scope, myservice) {
     console.log('setup');
-    $scope.a = 'my text';
     this.defaultTabIndex = 0;
     this.onUpdate = function(key) {
         return function(value) {
@@ -17,20 +16,4 @@ app.controller('controller', ['$scope', function($scope, myservice) {
         console.log("Main button clicked - switch to tab #1");
         $scope.refs.panelTabs.setActiveTab(1);
     };
-    var a = function ()
-    {
-      $scope.b = [
-       {value: '1', label: 'Enter expand mode'},
-       {value: '2', label: 'Show on full screen'},
-       {value: '3', label: 'Do nothing'}
-     ];
-      Wix.getSitePages(function(data) {
-        $scope.b = [
-         {value: '1', label: 'Enter expand mode'},
-         {value: '2', label: 'Show on full screen'},
-         {value: '3', label: 'Do nothing'}
-       ];
-      })
-    }
-    a();
 }]);
