069631b1a4f81027b431b931a4692880cdfb8e5b
Dor Galam <dorg@wix.com>
2017-07-13 12:21:38 +0300

    added components to widget, changed colors, added functionality

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -1,36 +1,43 @@
 require('./widget.scss');
-var Wix = require('Wix');
-var $ = require('jquery');
-Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, onSettingsUpdate);
+var app = angular.module('StarterApp', []);
 
-// You can get the style params programmatically, un-comment the following snippet to see how it works:
-/*Wix.Styles.getStyleParams(style => {
- console.log(style);
- });*/
+app.controller('WidgetCtrl', ['$scope', '$document', function($scope, $document) {
 
-// You can also get the style every time it changes, try this:
-/*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
- console.log(style);
- });*/
+  Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, onSettingsUpdate);
+  // You can get the style params programmatically, un-comment the following snippet to see how it works:
+  /*Wix.Styles.getStyleParams(style => {
+  console.log(style);
+  });*/
 
-function onSettingsUpdate(update) {
+  // You can also get the style every time it changes, try this:
+  /*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
+  console.log(style);
+  });*/
+  $scope.navToHome = () => {
+    Wix.getSiteMap(pages => {
+      Wix.navigateToPage(pages[0].pageId.substring(1));
+    });
+  }
+
+  $scope.showBox = false;
+
+  function onSettingsUpdate(update) {
+    $scope.showBox = true;
     update = stringify(update);
+    $scope.settingsUpdate = update;
+    $scope.$apply();
     console.log('SETTINGS UPDATED')
-    $('.json').html(update);
     updateCompHeight();
-}
-
-function updateCompHeight(height) {
-    const desiredHeight = height || document.documentElement.scrollHeight;
+  }
+  function updateCompHeight(height) {
+    const desiredHeight = height || $document[0].documentElement.scrollHeight;
     Wix.setHeight(desiredHeight);
-}
-
-function stringify(input) {
+  }
+  function stringify(input) {
     try {
-        return JSON.stringify(input, null, 4);
+      return JSON.stringify(input, null, 4);
     } catch (err) {
-        return input;
+      return input;
     }
-}
-
-
+  }
+}]);
